ad01e90015a31f0d2500fc621d15bdfbd993a767
ARIFUL ISLAM <a.islam@stscapital.lt>
2020-05-06 04:04:15 +0600

    search bar

--- src/app/share/appconstant/appconstant.ts
+++ src/app/share/appconstant/appconstant.ts
@@ -1,5 +1,5 @@
 export class AppConstant {
-    static ApiUrlLocal = 'http://localhost:5200';
-   // static ApiUrlLocal = 'https://discovermeapi.azurewebsites.net';
+   // static ApiUrlLocal = 'http://localhost:5200';
+    static ApiUrlLocal = 'https://discovermeapi.azurewebsites.net';
    public static NotLoginMessage = 'You are not login user.';
 }
